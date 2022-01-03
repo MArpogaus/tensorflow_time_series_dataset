f7aed8086b8ad96ccc20eed185cc103332a58474
Marcel Arpogaus <marcel.arpogaus@htwg-konstanz.de>
2022-01-03 16:49:35 +0100

    extends test for multiple batch and windows sizes

--- tests/test_pipleine.py
+++ tests/test_pipleine.py
@@ -1,26 +1,75 @@
+import numpy as np
 import pytest
 import tensorflow as tf
-import numpy as np
-
-from tensorflow_time_series_dataset.pipeline.patch_generator import PatchGenerator
 from tensorflow_time_series_dataset.pipeline.batch_processor import BatchPreprocessor
+from tensorflow_time_series_dataset.pipeline.patch_generator import PatchGenerator
 from tensorflow_time_series_dataset.preprocessors.groupby_dataset_generator import (
     GroupbyDatasetGenerator,
 )
 
 
-def get_value(l, c):
-    return int(f"{c:02d}{l:04d}")
+def get_id_and_idx(val):
+    id = val // 1e6
+    idx = val % 1e4
+    return id, idx
 
 
-def sorted_ds():
-    patch = [
-        [gen_value(int(id), c, l) for c in patch_columns]
-        for l in range(line, line + patch_size)
-    ]
-    return np.float32(patch)
+def gen_patch(df, idx, size):
+    return df.values[np.int(idx) : np.int(idx + size)]
 
 
+def gen_batch(df, columns, size, ids, lines):
+    batch = []
+    print(columns)
+    for id, line in zip(ids, lines):
+        if "id" in df.columns:
+            p = gen_patch(df[df.id == id][columns], line, size)
+        else:
+            p = gen_patch(df[columns], line, size)
+        batch.append(p)
+    return np.float32(batch)
+
+
+def validate_batch(
+    df,
+    ds,
+    batch_size,
+    history_size,
+    prediction_size,
+    history_columns,
+    meta_columns,
+    prediction_columns,
+):
+    x1_shape = (batch_size, history_size, len(history_columns))
+    x2_shape = (batch_size, 1, len(meta_columns))
+    y_shape = (batch_size, prediction_size, len(meta_columns))
+    for b, ((x1, x2), y) in enumerate(ds.as_numpy_iterator()):
+        assert x1.shape == x1_shape, f"Wrong shape: history ({b})"
+        assert x2.shape == x2_shape, f"Wrong shape: meta ({b})"
+        assert y.shape == y_shape, f"Wrong shape: prediction ({b})"
+        first_val = x1[:, 0]
+        ids, lines = get_id_and_idx(first_val)
+
+        assert np.all(
+            x1 == gen_batch(df, history_columns, history_size, ids, lines)
+        ), f"Wrong data: history ({b})"
+        assert np.all(
+            x2 == gen_batch(df, meta_columns, 1, ids, lines + history_size)
+        ), f"Wrong data: meta ({b})"
+
+        print(x1.shape)
+        y_test = x1[:, -prediction_size:] + prediction_size
+        assert np.all(y == y_test), f"Wrong data: prediction ({b})"
+
+        first_val = y[:, 0]
+        ids, lines = get_id_and_idx(first_val)
+
+        assert np.all(
+            y == gen_batch(df, prediction_columns, prediction_size, ids, lines)
+        ), f"Wrong data: prediction ({b})"
+
+
+# FIXTURES ####################################################################
 @pytest.fixture
 def groupby_dataset(time_series_df_with_id):
     time_series_df_with_id.set_index("date_time", inplace=True)
@@ -39,9 +88,20 @@ def patched_dataset(request, time_series_df):
         PatchGenerator(window_size=window_size, shift=shift),
         num_parallel_calls=tf.data.experimental.AUTOTUNE,
     )
-    return ds, df
+    return ds, df, window_size, shift
 
 
+@pytest.fixture(params=list(range(0, 48 * 4, 48)))
+def history_size(request):
+    return request.param
+
+
+@pytest.fixture(params=[4, 32])
+def batch_size(request):
+    return request.param
+
+
+# TESTS #######################################################################
 @pytest.mark.parametrize("window_size,shift", [(2 * 48, 48), (48 + 1, 1)])
 def test_patch_generator(time_series_df, window_size, shift):
     df = time_series_df.set_index("date_time")
@@ -95,80 +155,34 @@ def test_patch_generator_groupby(groupby_dataset, window_size, shift):
     assert i + 1 == patches, "Not enough patches"
 
 
-def get_id_and_idx(val):
-    id = val // 1e6
-    idx = val % 1e4
-    return id, idx
-
-
-def gen_patch(df, idx, size):
-    return df.values[np.int(idx) : np.int(idx + size)]
-
-
-def gen_batch(df, columns, size, ids, lines):
-    batch = []
-    print(columns)
-    for id, line in zip(ids, lines):
-        if "id" in df.columns:
-            p = gen_patch(df[df.id == id][columns], line, size)
-        else:
-            p = gen_patch(df[columns], line, size)
-        batch.append(p)
-    return np.float32(batch)
-
-
-def validate_batch(
-    df,
-    ds,
-    history_size,
-    history_columns,
-    meta_columns,
-    prediction_columns,
-):
-    for b, ((x1, x2), y) in enumerate(ds.as_numpy_iterator()):
-        prediction_size = y.shape[1]
-
-        first_val = x1[:, 0]
-        ids, lines = get_id_and_idx(first_val)
-
-        assert np.all(
-            x1 == gen_batch(df, history_columns, history_size, ids, lines)
-        ), f"Wrong data: history ({b})"
-        assert np.all(
-            x2 == gen_batch(df, meta_columns, 1, ids, lines + history_size)
-        ), f"Wrong data: meta ({b})"
-
-        print(x1.shape)
-        y_test = x1[:, -prediction_size:] + prediction_size
-        assert np.all(y == y_test), f"Wrong data: prediction ({b})"
-
-        first_val = y[:, 0]
-        ids, lines = get_id_and_idx(first_val)
-
-        assert np.all(
-            y == gen_batch(df, prediction_columns, prediction_size, ids, lines)
-        ), f"Wrong data: prediction ({b})"
-
-
 @pytest.mark.parametrize(
     "patched_dataset", [(2 * 48, 48), (48 + 1, 1)], indirect=["patched_dataset"]
 )
-def test_batch_processor(patched_dataset):
-    ds, df = patched_dataset
+def test_batch_processor(patched_dataset, history_size, batch_size):
+    history_size = 48
+    batch_size = 4
+    ds, df, window_size, shift = patched_dataset
+    predcition_size = window_size - history_size
 
     columns = list(sorted(df.columns))
 
     batch_kwds = dict(
-        history_size=48,
+        history_size=history_size,
         history_columns=columns[:1],
         meta_columns=columns[1:],
         prediction_columns=columns[:1],
     )
     print(batch_kwds)
-    ds_batched = ds.batch(4, drop_remainder=True)
+    ds_batched = ds.batch(batch_size, drop_remainder=True)
     ds_batched = ds_batched.map(
         BatchPreprocessor(**batch_kwds),
         num_parallel_calls=tf.data.experimental.AUTOTUNE,
     )
 
-    validate_batch(df, ds_batched, **batch_kwds)
+    validate_batch(
+        df,
+        ds_batched,
+        batch_size=batch_size,
+        prediction_size=predcition_size,
+        **batch_kwds,
+    )
